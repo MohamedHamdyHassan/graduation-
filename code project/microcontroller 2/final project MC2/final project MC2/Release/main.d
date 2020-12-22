b8879a5fa7d0ec03bd3f5bda583ebb38414025e3
MohamedHamdyHassan <you@example.com>
2020-12-22 20:45:32 +0200

    sending again

---
+++ code project/microcontroller 2/final project MC2/final project MC2/Release/main.d
@@ -0,0 +1,54 @@
+main.d main.o: .././main.c \
+ c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\Users\mns\Downloads\7.0\Packs\atmel\ATmega_DFP\1.3.300\include/avr/iom32.h \
+ c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ .././SPI.h .././ATMEGA32_Regs.h .././STD.h .././BIT_Math.h .././DIO.h \
+ .././BIT_MATH.h .././LED1.h .././LED.h .././Bit_math.h
+
+c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\Users\mns\Downloads\7.0\Packs\atmel\ATmega_DFP\1.3.300\include/avr/iom32.h:
+
+c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\users\mns\downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+.././SPI.h:
+
+.././ATMEGA32_Regs.h:
+
+.././STD.h:
+
+.././BIT_Math.h:
+
+.././DIO.h:
+
+.././BIT_MATH.h:
+
+.././LED1.h:
+
+.././LED.h:
+
+.././Bit_math.h:
